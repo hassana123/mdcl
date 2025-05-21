bb339e0ec043269ae29a6a2e0a13bf1cb654ba59
Hassana <hassanaabdll1@gmail.com>
2025-05-21 14:57:17 +0100

    second commit

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -1,5 +1,9 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, FreeMode } from 'swiper/modules';
+import 'swiper/css';
 
 const partnerImages = [
   '/mdcl/p1.png',
@@ -8,21 +12,37 @@ const partnerImages = [
   '/mdcl/p4.jpg',
   '/mdcl/p3.png',
   '/mdcl/p2.png',
+  '/mdcl/p7.png',
 ];
 
 const Partners = () => {
+  
+  const images = [...partnerImages, ...partnerImages];
   return (
-    <section className="w-[85%] mx-auto grid grid-cols-4 sm:grid-cols-3 md:grid-cols-6 gap-6 place-items-center my-10">
-      {partnerImages.map((src, index) => (
-        <Image
-          key={index}
-          src={src}
-          alt={`Partner ${index + 1}`}
-          width={150}
-          height={100}
-          className="object-contain "
-        />
-      ))}
+    <section className="w-full overflow-hidden py-8 bg-white">
+      <Swiper
+        modules={[Autoplay, FreeMode]}
+        slidesPerView={6}
+        spaceBetween={32}
+        loop
+        freeMode
+        speed={4000}
+        autoplay={{ delay: 0, disableOnInteraction: false }}
+        allowTouchMove={false}
+        className="w-full"
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index} className="flex items-center justify-center">
+            <Image
+              src={src}
+              alt={`Partner ${index + 1}`}
+              width={150}
+              height={100}
+              className="object-contain"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
