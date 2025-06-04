5dd8a22cfc4f8e7cb02f643dd38d85e9898b860c
Hassana <hassanaabdll1@gmail.com>
2025-06-04 16:20:09 +0100

    fix

--- app/(public)/resources/faqs/page.jsx
+++ app/(public)/resources/faqs/page.jsx
@@ -40,7 +40,7 @@ const Faqs = () => {
        
 
         <div className="">
-          <h2 className="text-2xl text-center font-bold text-gray-800 mb-6">Frequently Asked Questions Sheets</h2>
+          <h2 className="text-2xl text-center font-bold text-gray-800 mb-6">Frequently Asked Questions</h2>
           
           <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6">
             {dummyFaqs.map((faq, index) => (
